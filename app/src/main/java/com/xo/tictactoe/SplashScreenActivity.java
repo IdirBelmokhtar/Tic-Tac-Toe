bbb5d20514243ec0c050082bb1c3a578845dd373
Belmokhtar Idir <88589019+IdirBelmokhtar@users.noreply.github.com>
2022-05-01 19:42:31 +0100

    main

--- app/src/main/java/com/xo/tictactoe/SplashScreenActivity.java
+++ app/src/main/java/com/xo/tictactoe/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                    Intent intent = new Intent(getApplicationContext(), SplashScreenActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
             }
